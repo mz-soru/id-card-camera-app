493b9a87ac1ad533365acec331275d04935d89d7
mz-soru <soru@mz.co.kr>
2022-12-16 18:26:00 +0900

    svelte props & component test

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import TestComponent from "./TestComponent.svelte";
+	let number = 0
+	$: plusNumber = 'asd' + number
+	const increase = ()=>{number+=1}
+	const reset = ()=>{number=0}
+	$: numbers = [number*2,number*4,number*8]
+</script>
+
+<main>
+	<button on:click={increase}>button</button>
+	<TestComponent number={number} reset={reset}/>
+	<p>{number}</p>
+	<p>{plusNumber}</p>
+	{#each numbers as num}
+		{#if num%3===0}
+			<p>{num}</p>
+		{/if}
+	{/each}
+</main>
---
+++ src/TestComponent.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let number
+    export let reset
+</script>
+
+<main>
+    <button on:click={reset}>{number}</button>
+</main>
