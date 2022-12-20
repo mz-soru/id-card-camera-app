41f00e698488ad16786148bda7dfd52d37cce5ec
mz-soru <soru@mz.co.kr>
2022-12-20 17:38:48 +0900

    card layout 구현 & paint

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,93 @@
 <script lang="ts">
-	import TestComponent from "./TestComponent.svelte";
-	let number = 0
-	$: plusNumber = 'asd' + number
-	const increase = ()=>{number+=1}
-	const reset = ()=>{number=0}
-	$: numbers = [number*2,number*4,number*8]
+  import { onMount } from "svelte";
+  let IMAGE = {
+    width: 0,
+    height: 0,
+  };
+  const RATIO = 1.2;
+  $: edge = {
+    marginX: IMAGE.width / 4 / RATIO,
+    marginY: IMAGE.height / 4,
+    width: (IMAGE.width / 2) * RATIO,
+    height: IMAGE.height / 2,
+  };
+  let video: HTMLVideoElement = null;
+  let canvas: HTMLCanvasElement = null;
+  let imageEdge: HTMLElement = null;
+
+  const getVideoSource = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      video.srcObject = stream;
+      video.play();
+    } catch {
+      alert("에러가 발생했습니다.");
+    }
+  };
+
+  const drawImage = () => {
+    const picture = canvas.getContext("2d");
+    picture.clearRect(0, 0, IMAGE.width, IMAGE.height);
+    picture.drawImage(
+      video,
+      -edge.marginX,
+      -edge.marginY,
+      IMAGE.width,
+      IMAGE.height
+    );
+  };
+
+  const observer = new ResizeObserver((entries) => {
+    for (let entry of entries) {
+      const { width, height } = entry.contentRect;
+      IMAGE = { width, height };
+    }
+  });
+
+  onMount(async () => {
+    await getVideoSource();
+    observer.observe(video);
+  });
 </script>
 
 <main>
-	<button on:click={increase}>button</button>
-	<TestComponent number={number} reset={reset}/>
-	<p>{number}</p>
-	<p>{plusNumber}</p>
-	{#each numbers as num}
-		{#if num%3===0}
-			<p>{num}</p>
-		{/if}
-	{/each}
+  <div style="position:relative; ">
+    <!-- svelte-ignore a11y-media-has-caption -->
+    <div style="position: relative;">
+      <video autoplay playsinline bind:this={video} id="video" />
+      <div
+        id="videoEdge"
+        style="width:{edge.width}px; height:{edge.height}px; margin-left:{edge.marginX}px; margin-top:{edge.marginY}px"
+        bind:this={imageEdge}
+      />
+    </div>
+    <button on:click={drawImage}>Capture</button>
+  </div>
+  <canvas
+    id="canvas"
+    width={edge.width}
+    height={edge.height}
+    bind:this={canvas}
+  />
 </main>
+
+<style>
+  #video {
+    transform: rotate(0deg);
+    position: relative;
+    width: 100%;
+    max-width: 1000px;
+  }
+  #videoEdge {
+    z-index: 1;
+    position: absolute;
+    top: 0;
+    left: 0;
+    border: 2px solid red;
+    border-radius: 10px;
+  }
+  #canvas {
+    transform: rotate(0deg);
+    position: relative;
+  }
+</style>
--- src/TestComponent.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    export let number
-    export let reset
-</script>
-
-<main>
-    <button on:click={reset}>{number}</button>
-</main>
